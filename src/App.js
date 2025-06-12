4eba8b534bd7c5888f6ed5ae42fbda81d677404b
Samridhi Sinha <sinhasamridhi247@gmail.com>
2025-06-12 13:49:43 +0530

    Added React files and components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Sidebar from "./components/Sidebar";
+import Dashboard from "./components/Dashboard";
+import HealthTips from "./components/HealthTips";
+import 'leaflet/dist/leaflet.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="d-flex">
+        <Sidebar />
+        <div className="flex-grow-1 p-3 bg-dark text-light" style={{ minHeight: "100vh" }}>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/health-tips" element={<HealthTips />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
